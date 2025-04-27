25f3b6a4cc5b1c3f91422b38deba701e5ccd9fa0
roman <roman.r.daniel@gmail.com>
2025-04-28 09:41:04 +1000

    added an important test comment!!

--- src/main/java/net/river/hostilesinpeaceful/Hostilesinpeaceful.java
+++ src/main/java/net/river/hostilesinpeaceful/Hostilesinpeaceful.java
@@ -5,6 +5,7 @@ import net.fabricmc.api.ModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+// very important test comment
 public class Hostilesinpeaceful implements ModInitializer {
 	public static final String MOD_ID = "hostilesinpeaceful";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
